9a32d571a087a3570697bc4629e50873d45acc73
unknown <chasereynolds2019@gmail.com>
2022-08-22 10:39:13 -0500

    test

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -32,6 +32,7 @@ const App = () => {
       .getAll()
       .then(response => {
         console.log('promise fulfilled')
+        console.log('this is all the persons ', response.data)
         setPersons(response.data)
       })
   },[])
