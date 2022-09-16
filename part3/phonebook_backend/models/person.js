2ebf876cbd6ffacc8d5055660707fe57444078d3
unknown <chasereynolds2019@gmail.com>
2022-09-16 10:23:15 -0500

    adding the front and back end

---
+++ part3/phonebook_backend/models/person.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+const mongoose = require('mongoose')
+
+//const password = process.argv[2]
+
+const url = process.env.MONGODB_URL
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+    console.log(result)
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3
+  },
+  number: String,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
