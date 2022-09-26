3482352f50f2a835cb5493b227c1983bd73a01a8
unknown <chasereynolds2019@gmail.com>
2022-09-25 19:21:28 -0500

    part 4 complete and tokens are working. Now working on the asterik problems

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -6,8 +6,11 @@ const app = express()
 const cors = require('cors')
 require ('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+
 
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -26,6 +29,8 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
